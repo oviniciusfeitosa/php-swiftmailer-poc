593fdc5a3bf465cba8a625c4c10f89731575bfb1
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2019-10-31 18:05:28 -0300

    [FIX] Ajuste de definição de valor.

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ try {
         ->setFrom([getenv('V_REMETENTE')])
         ->setTo([getenv('V_DESTINATARIO')])
         ->setBody(getenv('V_CORPOEMAIL'));
-    $mailer = new Swift_Mailer($transport);
     $transport = new Swift_SmtpTransport(getenv('V_SMTPHOST'), getenv('V_SMTPPORT'));
     $transport->setUsername(getenv('V_USUARIO'))
         ->setPassword(getenv('V_SENHA'))
         ->setEncryption(getenv('V_CRIPTOGRAFIA'))
         ->setAuthMode(getenv('V_MODOAUTENTICACAO'));
+    $mailer = new Swift_Mailer($transport);
     $emailsEnviados = $mailer->send($message);
 
     xd("E-mails enviados : {$emailsEnviados}");
