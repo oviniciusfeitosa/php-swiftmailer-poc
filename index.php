829d7679afabe61f39b8a733585a1da255441061
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2019-10-31 17:21:06 -0300

    [UPDATE] Definições através de variáveis de ambiente.

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <?php
 
 try {
-
     require("vendor/autoload.php");
 
     ini_set('display_errors', true);
     error_reporting(E_ALL ^E_NOTICE);
 
     $message = new Swift_Message();
-    $message->setSubject($assunto)
-        ->setFrom($remetente)
-        ->setTo($destinatario)
-        ->setBody($corpoEmail);
+    $message->setSubject(getenv('V_ASSUNTO'))
+        ->setFrom(getenv('V_REMETENTE'))
+        ->setTo(getenv('V_DESTINATARIO'))
+        ->setBody(getenv('V_CORPOEMAIL'));
     $mailer = new Swift_Mailer($transport);
-    $transport = new Swift_SmtpTransport($smtpHost, $smtpPort);
-    $transport->setUsername($usuario)
-        ->setPassword($senha)
-        ->setEncryption($criptografia)
-        ->setAuthMode($modoAutenticacao);
-    $result = $mailer->send($message);
+    $transport = new Swift_SmtpTransport(getenv('V_SMTPHOST'), getenv('V_SMTPPORT'));
+    $transport->setUsername(getenv('V_USUARIO'))
+        ->setPassword(getenv('V_SENHA'))
+        ->setEncryption(getenv('V_CRIPTOGRAFIA'))
+        ->setAuthMode(getenv('V_MODOAUTENTICACAO'));
+    $emailsEnviados = $mailer->send($message);
 
-    xd("E-mails enviados : {$result}");
+    xd("E-mails enviados : {$emailsEnviados}");
 } catch (Exception $exception) {
-    xd($exception);
+    xd($exception->getMessage(), $exception->getTrace());
 }
